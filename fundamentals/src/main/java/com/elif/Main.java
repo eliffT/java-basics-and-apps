cfd031d1321a6f718de82e18f6a633e4aa0cdad1
eliffT <elifftasdemirr@gmail.com>
2025-06-04 01:39:41 +0300

    [JA-2] - Using ArrayList

--- fundamentals/src/main/java/com/elif/Main.java
+++ fundamentals/src/main/java/com/elif/Main.java
@@ -2,9 +2,21 @@ package com.elif;
 
 import com.elif.enumm.Days;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+    /*
         Days today = Days.WEDNESDAY;
         System.out.println("Today is: " + today);
+    */
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("İzmir");
+        cities.add("Ankara");
+        cities.add("İstanbul");
+
+        for (String city: cities){
+            System.out.println(city);
+        }
     }
 }
