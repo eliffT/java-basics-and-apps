983509a217a5b4d5dae995fb60a247989ad99d0f
eliffT <elifftasdemirr@gmail.com>
2025-06-04 02:02:02 +0300

    [JA-3] - Using HashSet

--- fundamentals/src/main/java/com/elif/Main.java
+++ fundamentals/src/main/java/com/elif/Main.java
@@ -6,6 +6,20 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Days today = Days.WEDNESDAY;
+        System.out.println("Today is: " + today);
+    */
+    /*
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("İzmir");
+        cities.add("Ankara");
+        cities.add("İstanbul");
+
+        for (String city: cities){
+            System.out.println(city);
+       }
+    */
         HashSet<String> fruits = new HashSet<>();
         fruits.add("Apple");
         fruits.add("Apple");
