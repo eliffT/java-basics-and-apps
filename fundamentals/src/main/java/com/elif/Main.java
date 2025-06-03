b18fc6b2f3152871d43b00c0d7f3f9359e92952c
eliffT <elifftasdemirr@gmail.com>
2025-06-04 02:38:59 +0300

    [JA-4] - Using HashMap

--- fundamentals/src/main/java/com/elif/Main.java
+++ fundamentals/src/main/java/com/elif/Main.java
@@ -2,9 +2,46 @@ package com.elif;
 
 import com.elif.enumm.Days;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        /*
         Days today = Days.WEDNESDAY;
         System.out.println("Today is: " + today);
+    */
+    /*
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("İzmir");
+        cities.add("Ankara");
+        cities.add("İstanbul");
+
+        for (String city: cities){
+            System.out.println(city);
+       }
+    */
+    /*
+        HashSet<String> fruits = new HashSet<>();
+        fruits.add("Apple");
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+
+        for (String fruit: fruits){
+            System.out.println(fruit);
+        }
+*/
+        Map<String, Integer> students = new HashMap<>();
+        students.put("Ahmet", 90);
+        students.put("Ayşe", 85);
+        students.put("Mehmet", 75);
+
+        for (Map.Entry<String, Integer> entry: students.entrySet()){
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
     }
 }
