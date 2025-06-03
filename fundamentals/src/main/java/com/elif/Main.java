69acbbb1954f89e3c7401c945f25057a7faee9e3
eliffT <elifftasdemirr@gmail.com>
2025-06-04 01:54:41 +0300

    [JA-3] - Using HashSet

--- fundamentals/src/main/java/com/elif/Main.java
+++ fundamentals/src/main/java/com/elif/Main.java
@@ -2,9 +2,18 @@ package com.elif;
 
 import com.elif.enumm.Days;
 
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        Days today = Days.WEDNESDAY;
-        System.out.println("Today is: " + today);
+        HashSet<String> fruits = new HashSet<>();
+        fruits.add("Apple");
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+
+        for (String fruit: fruits){
+            System.out.println(fruit);
+        }
     }
 }
