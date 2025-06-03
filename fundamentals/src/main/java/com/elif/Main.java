229ff762ded54458a1958c57313d38df6ff8b8c2
eliffT <elifftasdemirr@gmail.com>
2025-06-04 01:15:21 +0300

    [JA-1] - ENUM-Define Constants

--- fundamentals/src/main/java/com/elif/Main.java
+++ fundamentals/src/main/java/com/elif/Main.java
@@ -1,7 +1,10 @@
 package com.elif;
 
+import com.elif.enumm.Days;
+
 public class Main {
     public static void main(String[] args) {
-
+        Days today = Days.WEDNESDAY;
+        System.out.println("Today is: " + today);
     }
 }
