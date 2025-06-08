c6407d2d216c2b1e3998ce842ec9205a31da6b7e
eliffT <elifftasdemirr@gmail.com>
2025-06-08 22:15:48 +0300

    [JA-6] - Lambda Expressions

--- fundamentals/src/main/java/com/elif/Main.java
+++ fundamentals/src/main/java/com/elif/Main.java
@@ -2,9 +2,20 @@ package com.elif;
 
 import com.elif.enumm.Days;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Days today = Days.WEDNESDAY;
-        System.out.println("Today is: " + today);
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        numbers.forEach (System.out::println);
+
     }
 }
